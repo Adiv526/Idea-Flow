4bea81e102b54a8049e54ffd22695c51c6895be9
Adiv526 <32030783+Adiv526@users.noreply.github.com>
2026-01-19 02:32:31 +0530

    Update market.py

--- market.py
+++ market.py
@@ -1,1 +1,17 @@
+def market_prompt():
+    return """
+Evaluate the market sanity for this startup idea:
+
+{idea}
+
+Return JSON only:
+
+{
+  "market_type": "niche | medium | large | unclear",
+  "market_reasoning": "",
+  "trend_support": "",
+  "market_score": 1-10,
+  "market_assumption": ""
+}
+"""
 
