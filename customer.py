6934e1b644f63d6ff32a3973396be62446dd5294
Adiv526 <32030783+Adiv526@users.noreply.github.com>
2026-01-19 02:32:53 +0530

    Create customer_prompt function
    Add a function to prompt for ideal customer details in JSON format.

--- customer.py
+++ customer.py
@@ -1,1 +1,17 @@
+def customer_prompt():
+    return """
+Identify the strongest early adopter for this startup idea:
+
+{idea}
+
+Return JSON only:
+
+{
+  "ideal_customer": "",
+  "customer_context": "",
+  "why_they_care": "",
+  "early_adopter_strength": 1-10,
+  "wrong_customer_risk": ""
+}
+"""
 
