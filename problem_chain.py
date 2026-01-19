3805694fd53f6ea4880e02887ff91f2ef6e9f1e6
Adiv526 <32030783+Adiv526@users.noreply.github.com>
2026-01-19 17:07:45 +0530

    Update problem_chain.py

--- problem_chain.py
+++ problem_chain.py
@@ -1,19 +1,8 @@
-from langchain.prompts import PromptTemplate
-from llm import get_llm
-from problem import problem_prompt
+from llm import call_llm
+from problem_prompt import problem_prompt
 from parser import parse_json
 
-
-def run_problem_chain(idea: str):
-    llm = get_llm()
-
-    prompt = PromptTemplate(
-        template=problem_prompt(),
-        input_variables=["idea"]
-    )
-
-    chain = prompt | llm
-
-    response = chain.invoke({"idea": idea})
-
+def run_problem_chain(idea):
+    prompt = problem_prompt(idea)
+    response = call_llm(prompt)
     return parse_json(response)
