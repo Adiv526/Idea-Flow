0f9c5d2506ae86c2eb09c679ee4ee162c0b5bfba
Adiv526 <32030783+Adiv526@users.noreply.github.com>
2026-01-19 15:40:46 +0530

    Create risk.py

---
+++ risk.py
@@ -0,0 +1,15 @@
+def risk_prompt():
+    return """
+Identify the biggest risks for this startup idea:
+
+{idea}
+
+Return JSON only:
+
+{
+  "top_risks": [],
+  "hardest_part_to_execute": "",
+  "feasibility_score": 1-10,
+  "why_most_founders_fail_here": ""
+}
+"""
