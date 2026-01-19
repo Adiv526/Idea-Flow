6dedfb74737d753ad3812729d1f49c72696ae0db
Adiv526 <32030783+Adiv526@users.noreply.github.com>
2026-01-19 17:30:43 +0530

    Update summary_chain.py

--- summary_chain.py
+++ summary_chain.py
@@ -1,24 +1,10 @@
 from llm import call_llm
-from risk import risk_prompt
+from summary_prompt import summary_prompt
 from parser import parse_json
 
 def run_summary_chain(idea, problem, customer, market, solution, risk):
-    prompt = PromptTemplate(
-        input_variables=[
-            "idea", "problem", "customer",
-            "market", "solution", "risk"
-        ],
-        template=summary_prompt()
+    prompt = summary_prompt(
+        idea, problem, customer, market, solution, risk
     )
-
-   
-    response = chain.invoke({
-    "idea": idea,
-    "problem": problem,
-    "customer": customer,
-    "market": market,
-    "solution": solution,
-    "risk": risk
-})
+    response = call_llm(prompt)
     return parse_json(response)
-
