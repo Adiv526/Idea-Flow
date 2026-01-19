ba86507968e8ffa7fa61240d2c6e8698c27483b0
Adiv526 <32030783+Adiv526@users.noreply.github.com>
2026-01-19 15:34:14 +0530

    Create risk_chain.py

---
+++ risk_chain.py
@@ -0,0 +1,16 @@
+from langchain import LLMChain
+from llm import get_llm
+from risk import risk_prompt
+from parser import parse_json
+from langchain.prompts import PromptTemplate
+
+def run_risk_chain(idea):
+    prompt = PromptTemplate(
+        input_variables=["idea"],
+        template=risk_prompt()
+    )
+
+    chain = LLMChain(llm=get_llm(), prompt=prompt)
+    result = chain.run(idea=idea)
+
+    return parse_json(result)
