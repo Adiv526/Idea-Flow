6c3107bb4c1ce81eae4647baebfc9f27610473ac
Adiv526 <32030783+Adiv526@users.noreply.github.com>
2026-01-19 17:12:26 +0530

    Update risk_chain.py

--- risk_chain.py
+++ risk_chain.py
@@ -1,16 +1,10 @@
-from langchain import LLMChain
-from llm import get_llm
+
+from llm import call_llm
 from risk import risk_prompt
 from parser import parse_json
-from langchain.prompts import PromptTemplate
+
 
 def run_risk_chain(idea):
-    prompt = PromptTemplate(
-        input_variables=["idea"],
-        template=risk_prompt()
-    )
-
-    chain = LLMChain(llm=get_llm(), prompt=prompt)
-    result = chain.run(idea=idea)
-
-    return parse_json(result)
+    prompt = problem_prompt(idea)
+    response = call_llm(prompt)
+    return parse_json(response))
