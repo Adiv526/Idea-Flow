61b35cf5bf9ef91092bbc49376a80f10577f670c
Adiv526 <32030783+Adiv526@users.noreply.github.com>
2026-01-19 17:12:01 +0530

    Update market_chain.py

--- market_chain.py
+++ market_chain.py
@@ -1,16 +1,10 @@
-from langchain import LLMChain
-from llm import get_llm
+
+from llm import call_llm
 from market import market_prompt
 from parser import parse_json
-from langchain.prompts import PromptTemplate
+
 
 def run_market_chain(idea):
-    prompt = PromptTemplate(
-        input_variables=["idea"],
-        template=market_prompt()
-    )
-
-    chain = LLMChain(llm=get_llm(), prompt=prompt)
-    result = chain.run(idea=idea)
-
-    return parse_json(result)
+  prompt = problem_prompt(idea)
+    response = call_llm(prompt)
+    return parse_json(response)
