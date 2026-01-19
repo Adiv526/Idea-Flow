03791f8a6e61f7e6a2230363cef47d1eda84729f
Adiv526 <32030783+Adiv526@users.noreply.github.com>
2026-01-19 16:09:23 +0530

    Update llm.py

--- llm.py
+++ llm.py
@@ -1,7 +1,10 @@
-from langchain.llms import HuggingFaceHub
+from langchain_community.llms import HuggingFaceEndpoint
+import os
 
 def get_llm():
-    return HuggingFaceHub(
-        repo_id="mistralai/Mistral-7B-Instruct-v0.1",
-        model_kwargs={"temperature": 0.3}
+    return HuggingFaceEndpoint(
+        repo_id="mistralai/Mistral-7B-Instruct-v0.2",
+        temperature=0.3,
+        max_new_tokens=600,
+        huggingfacehub_api_token=os.environ.get("HUGGINGFACEHUB_API_TOKEN"),
     )
