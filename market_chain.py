9c058245f5c6af32080fb079acaa89649e687e78
Adiv526 <32030783+Adiv526@users.noreply.github.com>
2026-01-19 17:17:29 +0530

    Update market_chain.py

--- market_chain.py
+++ market_chain.py
@@ -5,6 +5,6 @@ from parser import parse_json
 
 
 def run_market_chain(idea):
-  prompt = problem_prompt(idea)
+    prompt = problem_prompt(idea)
     response = call_llm(prompt)
     return parse_json(response)
