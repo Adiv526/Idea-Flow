d7ec76cb75afae36f930627c746c998229b16e54
Adiv526 <32030783+Adiv526@users.noreply.github.com>
2026-01-19 15:37:08 +0530

    Create summary_chain.py

---
+++ summary_chain.py
@@ -0,0 +1,26 @@
+from langchain import LLMChain
+from llm import get_llm
+from risk import risk_prompt
+from parser import parse_json
+from langchain.prompts import PromptTemplate
+
+def run_summary_chain(idea, problem, customer, market, solution, risk):
+    prompt = PromptTemplate(
+        input_variables=[
+            "idea", "problem", "customer",
+            "market", "solution", "risk"
+        ],
+        template=summary_prompt()
+    )
+
+    chain = LLMChain(llm=get_llm(), prompt=prompt)
+    result = chain.run(
+        idea=idea,
+        problem=problem,
+        customer=customer,
+        market=market,
+        solution=solution,
+        risk=risk
+    )
+
+    return parse_json(result)
