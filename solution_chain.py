80234b74dd368c87b680d5c18b9e45868d5bcb46
Adiv526 <32030783+Adiv526@users.noreply.github.com>
2026-01-19 17:12:51 +0530

    Update solution_chain.py

--- solution_chain.py
+++ solution_chain.py
@@ -1,16 +1,8 @@
-from langchain import LLMChain
 from llm import get_llm
 from solution import solution_prompt
 from parser import parse_json
-from langchain.prompts import PromptTemplate
 
 def run_solution_chain(idea):
-    prompt = PromptTemplate(
-        input_variables=["idea"],
-        template=solution_prompt()
-    )
-
-    chain = LLMChain(llm=get_llm(), prompt=prompt)
-    result = chain.run(idea=idea)
-
-    return parse_json(result)
+    prompt = problem_prompt(idea)
+    response = call_llm(prompt)
+    return parse_json(response)
