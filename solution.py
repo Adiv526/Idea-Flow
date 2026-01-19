56463505d4fc1118b148c8885641ca38615e2f8b
Adiv526 <32030783+Adiv526@users.noreply.github.com>
2026-01-19 15:41:13 +0530

    Create solution.py

---
+++ solution.py
@@ -0,0 +1,16 @@
+def solution_prompt():
+    return """
+Evaluate the proposed solution approach:
+
+{idea}
+
+Return JSON only:
+
+{
+  "solution_summary": "",
+  "differentiation_strength": 1-10,
+  "is_10x_better_candidate": "yes | maybe | no",
+  "time_to_value": "immediate | short | long",
+  "solution_risk": ""
+}
+"""
