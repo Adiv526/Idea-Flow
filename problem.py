e5a3bc1264a03aa0d2f4807fe88cd131ed226af5
Adiv526 <32030783+Adiv526@users.noreply.github.com>
2026-01-19 16:36:52 +0530

    Update problem.py

--- problem.py
+++ problem.py
@@ -2,17 +2,18 @@ def problem_prompt():
     return """
 You are a startup analyst.
 
-Evaluate the PROBLEM behind this idea:
+Evaluate the PROBLEM behind this startup idea:
 
 {idea}
 
 Return JSON only:
-{
+
+{{
   "problem_summary": "",
-  "pain_intensity": "",
+  "pain_intensity": "low | medium | high",
   "problem_clarity_score": 1-10,
   "current_workarounds": [],
   "key_assumption": "",
   "validation_question": ""
-}
+}}
 """
