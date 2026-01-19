445421eeb60631699d062af66758da2568b7ea5b
Adiv526 <32030783+Adiv526@users.noreply.github.com>
2026-01-19 16:38:20 +0530

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,11 +6,11 @@ Evaluate the proposed solution approach:
 
 Return JSON only:
 
-{
+{{
   "solution_summary": "",
   "differentiation_strength": 1-10,
   "is_10x_better_candidate": "yes | maybe | no",
   "time_to_value": "immediate | short | long",
   "solution_risk": ""
-}
+}}
 """
